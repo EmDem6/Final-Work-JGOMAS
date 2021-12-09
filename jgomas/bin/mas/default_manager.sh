091f137723e71da1ccd59679c36c7f91cb40d04e
usainzg <unaihtc70@gmail.com>
2021-12-09 17:55:07 +0100

    Add Pi314 and Lucky for testing and clean launchers

--- jgomas/bin/mas/default_manager.sh
+++ jgomas/bin/mas/default_manager.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-java -classpath "lib/jade.jar:lib/jadeTools.jar:lib/Base64.jar:lib/http.jar:lib/iiop.jar:lib/beangenerator.jar:lib/jgomas.jar:lib/jason.jar:lib/JasonJGomas.jar:classes:." jade.Boot -gui -host 127.0.0.1 -port 1099 "Manager:es.upv.dsic.gti_ia.jgomas.CManager(14,map_04,125,10)"
+java -classpath "lib/jade.jar:lib/jadeTools.jar:lib/Base64.jar:lib/http.jar:lib/iiop.jar:lib/beangenerator.jar:lib/jgomas.jar:lib/jason.jar:lib/JasonJGomas.jar:classes:." jade.Boot -gui -host 127.0.0.1 "Manager:es.upv.dsic.gti_ia.jgomas.CManager(14,map_04,125,10)"
---
+++ jgomas/bin/mas/default_manager_linux.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+java -classpath "lib/jade.jar:lib/jadeTools.jar:lib/Base64.jar:lib/http.jar:lib/iiop.jar:lib/beangenerator.jar:lib/jgomas.jar:lib/jason.jar:lib/JasonJGomas.jar:classes:." jade.Boot -gui -local-host 127.0.0.1 "Manager:es.upv.dsic.gti_ia.jgomas.CManager(14,map_04,125,10)"
