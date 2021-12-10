933b49f86ea45243b580b097914afd2ce12befbd
Unai <unaihtc70@gmail.com>
2021-12-10 14:41:10 +0100

    Add some .bat files for testing

---
+++ jgomas/bin/mas/L_vsLucky.bat
@@ -0,0 +1,9 @@
+set rutaAxis="srcLucky"
+set tipoAxis=%rutaAxis%"/jasonAgent_AXIS.asl"
+set tipoAllied="jasonAgent_ALLIED.asl"
+set tipoAllied2="jasonAgent_ALLIED_MEDIC.asl"
+set tipoAllied3="jasonAgent_ALLIED_FIELDOPS.asl"
+set log="logs/log_vsLucky.txt"
+set Axis="Corralo:es.upv.dsic.gti_ia.JasonJGomas.BasicTroopJasonArch(%tipoAxis%);Lechuga:es.upv.dsic.gti_ia.JasonJGomas.BasicTroopJasonArch(%tipoAxis%);Adrian:es.upv.dsic.gti_ia.JasonJGomas.BasicTroopJasonArch(%tipoAxis%);Edu:es.upv.dsic.gti_ia.JasonJGomas.BasicTroopJasonArch(%tipoAxis%);Elastico:es.upv.dsic.gti_ia.JasonJGomas.BasicTroopJasonArch(%tipoAxis%);LaCosa:es.upv.dsic.gti_ia.JasonJGomas.BasicTroopJasonArch(%tipoAxis%);Fuego:es.upv.dsic.gti_ia.JasonJGomas.BasicTroopJasonArch(%tipoAxis%)"
+set EQUIPOAllied="E1:es.upv.dsic.gti_ia.JasonJGomas.BasicTroopJasonArch(%tipoAllied%);E2:es.upv.dsic.gti_ia.JasonJGomas.BasicTroopJasonArch(%tipoAllied%);E3:es.upv.dsic.gti_ia.JasonJGomas.BasicTroopJasonArch(%tipoAllied%);E4:es.upv.dsic.gti_ia.JasonJGomas.BasicTroopJasonArch(%tipoAllied%);E5:es.upv.dsic.gti_ia.JasonJGomas.BasicTroopJasonArch(%tipoAllied%);E6:es.upv.dsic.gti_ia.JasonJGomas.BasicTroopJasonArch(%tipoAllied%);E7:es.upv.dsic.gti_ia.JasonJGomas.BasicTroopJasonArch(%tipoAllied%)"
+java -classpath "lib\jade.jar;lib\jadeTools.jar;lib\Base64.jar;lib\http.jar;lib\iiop.jar;lib\beangenerator.jar;lib\jgomas.jar;lib\jason.jar;lib\JasonJGomas.jar;classes;." jade.Boot -container -host localhost %EQUIPOAllied%;%Axis% > %log%
